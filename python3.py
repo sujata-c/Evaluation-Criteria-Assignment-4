0c00f662e2e570ed4efcb6e32208552ed2561b4f
sujata-c <sujata.c3001@gmail.com>
2021-02-05 16:22:34 +0530

    complete1

--- python3.py
+++ python3.py
@@ -1,11 +1,11 @@
 def menu():
-    print "Menu \n 1. Celsius to Farenheit \n2. Farenheit to Celsius \n 3. Exit"
+    print("Menu \n 1. Celsius to Farenheit \n2. Farenheit to Celsius \n 3. Exit")
 
     try:
-        choice = int(raw_input("Enter your choice "))  # 2only
+        choice = int(input("Enter your choice "))
 
-    except ValueError, e:  #2only
-        print e             # 2only
+    except ValueError as e:
+        print(e)
         choice = 0
 
     return choice
@@ -15,20 +15,20 @@ if __name__ == '__main__':
     choice = menu()
     while choice != 3:
         if choice is 1:
-            count = int(raw_input("How many temperature conversions?"))
-            for i in xrange(0,count):           # 2only
-                Celsius = int(raw_input("Enter a temperature in Celsius: "))
+            count = int(input("How many temperature conversions?"))
+            for i in range(0,count):           # 2only
+                Celsius = int(input("Enter a temperature in Celsius: "))
                 Fahrenheit = 9.0 / 5.0 * Celsius + 32
-                print "Temperature: Celsius", Celsius , "Fahrenheit: ", Fahrenheit
+                print("Temperature: Celsius", Celsius , "Fahrenheit: ", Fahrenheit)
 
         elif choice == 2:
-            count = int(raw_input("How many temperature conversions?"))
-            for i in xrange(0, count):
-                Fahrenheit = int(raw_input("Enter a temperature in Fahrenheit: "))
+            count = int(input("How many temperature conversions?"))
+            for i in range(0, count):
+                Fahrenheit = int(input("Enter a temperature in Fahrenheit: "))
                 Celsius = Fahrenheit - 32 / 1.8000
-                print "Temperature: Celsius", Celsius , "Fahrenheit: ", Fahrenheit
+                print("Temperature: Celsius", Celsius , "Fahrenheit: ", Fahrenheit)
         else:
-            print "Try Again!!"
+            print("Try Again!!")
 
         choice = menu()
 
